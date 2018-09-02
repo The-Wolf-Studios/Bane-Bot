81e59260685fd7a4f088138a7dad557dd0036960
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-09-02 01:34:53 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+var bot = new Discord.Client();
+const config = require(`./config.json`);
+const staff = require(`./team.json`);
+const fs = require('fs');
+var bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+  console.log('Finding Commands.');
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+
+bot.on("ready" () => {
+  bot.user.setActivity(`Supporting ${bot.guilds.size}, servers!`)
+  console.log('Bane-Bot is ready!!!')
+})
+
+bot.on('message', message => {
+  let mArray = message.content.split(" ")
+  let args = mArray.slice(1)
+  let cmd = bot.commands.get(mArray[0].slice(prefix.length))
+  if (message.author.bot) return;
+  if (message.channel.type == "dm") return;
+  if (!message.content.startsWith(prefix)) return;
+  
+  if (cmd) {
+    cmd.run(bot, message, args, discord)
+  }
+})
+bot.login(process.env.TOKEN)
