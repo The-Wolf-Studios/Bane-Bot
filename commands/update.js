86568d2f3e3a93f13463af5f3bf235d6902e04d2
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-09-05 22:47:27 -0400

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -1,12 +1,15 @@
+const developerids = require(`../team.json`)
 module.exports.run = (bot, message, args, discord) => {
-let joinargs = args.join(" ");
-let em = new discord.RichEmbed()
-  .setTitle('Update Notes!')
-  .setDescription(joinargs)
-  .setFooter(`Sent by ${message.author.username}`)
-  .setColor("BLUE")
+  if(developerids.includes(message.author.id)) {
+  let joinargs = args.join(" ");
+  let em = new discord.RichEmbed()
+    .setTitle('Update Notes!')
+    .setDescription(joinargs)
+    .setFooter(`Sent by ${message.author.username}`)
+    .setColor("BLUE")
   
   message.channel.send({embed: em})
+  }
 }
 
 module.exports.help = {
