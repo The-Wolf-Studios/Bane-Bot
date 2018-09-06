55e9a078aafbe6cfa04cc835cedaa857ebf1c06d
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-09-05 22:12:23 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,7 @@ module.exports.run = (bot, message, args, discord) => {
   let embed = new discord.RichEmbed()
   .setTitle("Bane Bot Help Menu!")
   .setDescription("This will be a list of all of Bane Bot's commands! o~o")
+  .setColor("GREEN")
   .addField("Bot Help", "Help")
   
   message.channel.send({embed: embed})
