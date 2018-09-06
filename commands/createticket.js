f2e7cb805d721c295d1e47846363daddc8be8f4b
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-09-06 04:56:08 -0400

    Update createticket.js

--- commands/createticket.js
+++ commands/createticket.js
@@ -1,6 +1,6 @@
 module.exports.run = (bot, message, args, discord) => {
-  let issue = args.join(" ")
-  let emb = discord.RichEmbed()
+  let issue = args.join(" ");
+  let emb = discord.RichEmbed();
   .setTitle("Support Ticket.")
   .setDescription(issue)
   .setColor("GREEN")
