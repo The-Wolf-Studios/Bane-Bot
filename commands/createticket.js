bbd4e24c1ece6d87dc36913ac5eacea0c4b6cac9
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-09-06 04:49:44 -0400

    Create createticket.js

---
+++ commands/createticket.js
@@ -0,0 +1,11 @@
+module.exports.run = (bot, message, args, discord) => {
+  let issue = args.join(" ")
+  let emb = discord.RichEmbed()
+  .setTitle("Support Ticket.")
+  .setColor("GREEN")
+  .setDescription(issue)
+  .addField("Ticket Number", Math.floor(Math.random()*1000))
+  .setFooter(message.author.username)
+  
+  messages.channel.send({embed: emb})
+}
