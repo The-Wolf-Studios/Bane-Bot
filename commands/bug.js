8bccbb682a2bf59601dd4ff2318c3462cc8c8edb
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-09-05 23:17:59 -0400

    Create bug.js

---
+++ commands/bug.js
@@ -0,0 +1,23 @@
+module.exports.run = (bot, message, args, discord) => {
+    var mes = args.join(' ')
+    if (!mes) return message.channel.send("You need to provide a message.")
+    let em = new discord.RichEmbed()
+        .setTitle(`Contact System`)
+        .setDescription(`\n${mes}`)
+        .setColor(`RED`)
+        .setFooter(`This message was sent by ${message.author.tag}`)
+        .setTimestamp()
+    let emb = new discord.RichEmbed()
+        .setTitle(`Contact System`)
+        .setDescription(`Now contacting WolfBane...`)
+        .setColor(`BLUE`)
+        .setFooter(`Contact System`)
+        .setTimestamp()
+        .setThumbnail(bot.user.avatarURL)
+        
+    message.channel.send({embed: emb})
+    bot.users.get('369256915479560192').send({embed: em})
+}
+module.exports.help = {
+    name: "contact"
+}
