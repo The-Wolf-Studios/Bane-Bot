c346b8dfdea55663f2c3f2d011098f712c4380e3
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-09-05 22:16:32 -0400

    Added Eval

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,7 @@ module.exports.run = (bot, message, args, discord) => {
   .setDescription("This will be a list of all of Bane Bot's commands! o~o")
   .setColor("GREEN")
   .addField("Bot Help", "Help")
+  .addField("Developer Only", "Eval")
   
   message.channel.send({embed: embed})
 }
